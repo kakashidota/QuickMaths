9640e783d5fc8f88e927f58c2da396585cb15296
kakashidota <robin.kamo@live.se>
2017-11-13 21:29:44 +0100

    Addquestion activity

--- app/src/main/java/se/iths/apostolidis/quickmaths/AddQuestionActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/AddQuestionActivity.java
@@ -3,12 +3,11 @@ package se.iths.apostolidis.quickmaths;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class AddQuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_add_question);
     }
 }
-    
