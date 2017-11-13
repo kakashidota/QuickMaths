9452f5cbafd4d1897a2fe28964b7bf4b53fa6d96
kakashidota <robin.kamo@live.se>
2017-11-13 21:49:43 +0100

    Testing Git

--- app/src/main/java/se/iths/apostolidis/quickmaths/MainActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void testMethod() {
         //Tjena boys
+        //Funkar det?
     }
 
     public void testearen(){
@@ -22,4 +23,5 @@ public class MainActivity extends AppCompatActivity {
     public void knapee (){
         int knape = 0;
     }
+
 }
