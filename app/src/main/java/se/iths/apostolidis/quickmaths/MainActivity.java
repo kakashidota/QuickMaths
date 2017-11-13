4ef1ef52b698066ff9d5ecae3d6fe127bc626875
kakashidota <robin.kamo@live.se>
2017-11-13 13:57:30 +0100

    Test från kakkkkkaaaaa

--- app/src/main/java/se/iths/apostolidis/quickmaths/MainActivity.java
+++ app/src/main/java/se/iths/apostolidis/quickmaths/MainActivity.java
@@ -14,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
     public void testMethod() {
         //Tjena boys
     }
+
+    public void testearen(){
+        //knape är vacker
+    }
 }
